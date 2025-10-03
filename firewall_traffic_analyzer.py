9b0e03631ab86d2501aaead472c23be88e4d2d72
justinweaver1092-bit <justin.weaver1092@gmail.com>
2025-10-02 23:30:24 -0400

    Update firewall_traffic_analyzer.py

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -18,7 +18,7 @@ print('=== Network Traffic Security Analyzer ===')
 port_num=(int(input('Enter the port number (e.g., 80, 22, 443, 3389): ')))
 data_size=(int(input('Enter the data transfer size in megabytes (MB): ')))
 print('FIREWALL LOG:')
-print(f'Port:{port_num}, Transfer Size:{data_size}MB')
+print(f'Port:{port_num}, Transfer Size:{data_size} MB')
 if port_num == 22 and data_size <= 100:
     print ("HIGH RISK: Potential unauthorized remote access detected!")
 elif port_num == 80 or data_size  >= 100:
